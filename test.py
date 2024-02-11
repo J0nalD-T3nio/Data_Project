86a7dc4df1ede55d684d48b0b7e2f1cca17e6786
J0nalD-T3nio <onietpogi@gmail.com>
2024-02-12 01:17:45 +0800

    Added test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+"""    <EDIT THIS FOR MODULE DOCSTRING>   """
+
+def main():
+    """
+        _summary_
+    """
+    print('Hello World, This is Jonald')
+
+if __name__ == '__main__':
+    main()
